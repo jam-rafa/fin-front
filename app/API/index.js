518e0f439ec15db54e25906710e9e57b43a06977
jam-rafa <rafa.deandrade35@gmail.com>
2024-08-13 22:21:23 -0300

    mudanca de rota

--- app/API/index.js
+++ app/API/index.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://fintech-y3e0.onrender.com/api",
+
 });
 
 export default Api;
